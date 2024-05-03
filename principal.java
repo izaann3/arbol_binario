80c0a56d691dcf9030696047eb3fcc596de9d658
Izan Lozano <151909866+izaann3@users.noreply.github.com>
2024-05-03 09:53:23 +0200

    ponemos el contenido del main

--- principal.java
+++ principal.java
@@ -1,5 +1,11 @@
 package binario;
 
-public class Principal {
+public class Principal{
+        public static void main(String[] args){
+            ArbreBinariStrings arbre = new ArbreBinariStrings("A");
 
+            arbre.afegirderecha("B");
+            arbre.afegirderecha("C");
+            arbre.afegirderecha("D");
+        }
 }
