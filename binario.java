fb1eb83b93a330c3a74973370d8f0f61d5c1fe97
Izan Lozano <151909866+izaann3@users.noreply.github.com>
2024-05-03 08:52:43 +0200

    creamos las classes privadas y publicas de las variables del arbol para tipo string. empezare pudiendo moverme a la derecha

--- binario.java
+++ binario.java
@@ -1,5 +1,19 @@
 package binario;
 
-public class ArbreBinari{}
-public class ArbreBinariEnters{}
-public class ArbreBinariStrings{}
+/*public class ArbreBinari{}
+public class ArbreBinariEnters{}*/
+
+public class ArbreBinariStrings{
+    private Node arrel;
+
+    private static class Node{
+        private String valor;
+        private Node dercha;
+
+        public Node(String Valor){
+            this.valor = valor;
+            this.filldret = null;
+        }
+    }
+
+}
